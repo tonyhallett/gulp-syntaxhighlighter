4ad22feafb065f0112a9ce891bce754bae747787
Tony Hallett <tonyhallett74@gmail.com>
2019-03-21 19:10:28 +0000

    refactor applySyntaxHighlighter

--- gulp-syntaxhighlighter/syntaxHighlighterDocumentManager.ts
+++ gulp-syntaxhighlighter/syntaxHighlighterDocumentManager.ts
@@ -27,33 +27,38 @@ export class SyntaxHighlighterDocumentManager implements ISyntaxHighlighterDocum
     private addTheme(theme: string) {
         this.jsDomDocument.addCss(theme);
     }
+    private getHighlightScript(globalParams:string,config:string){
+        
+return `
+function merge(obj1, obj2)
+{
 
+    var result = {}, name;
+    
+    for (name in obj1)
+        result[name] = obj1[name];
+
+    for (name in obj2)
+        result[name] = obj2[name];
+    
+
+    return result;
+};
+SyntaxHighlighter.config=merge(SyntaxHighlighter.config,${config})
+SyntaxHighlighter.highlight(${globalParams});
+`
+    }
     applySyntaxHighlighter(globalParams: SyntaxHighlighterOptions["globalParams"], config: SyntaxHighlighterOptions["config"]) {
         config = config ? config : {};
-        globalParams = globalParams ? globalParams : {};
         (globalParams as any).toolbar = false;
 
-        const highlightScript = `
-        function merge(obj1, obj2)
-        {
-            
-            var result = {}, name;
-            
-            for (name in obj1)
-                result[name] = obj1[name];
-
-            for (name in obj2)
-                result[name] = obj2[name];
-            
-
-            return result;
-        };
         
-        SyntaxHighlighter.config=merge(SyntaxHighlighter.config,${JSON.stringify(config)})
-       
-        SyntaxHighlighter.highlight(${JSON.stringify(globalParams)});
-        `
-        this.jsDomDocument.addSyntaxHighlighterScript(highlightScript);
+        this.jsDomDocument.addSyntaxHighlighterScript(
+            this.getHighlightScript(
+                JSON.stringify(globalParams),
+                JSON.stringify(config)
+            )
+        );
 
     }
 }
