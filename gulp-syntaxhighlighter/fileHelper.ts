70a5e8c4aad5a764717d73df6715a91c6f06be80
Tony Hallett <tonyhallett74@gmail.com>
2019-03-22 09:35:03 +0000

    rename file helper method

--- gulp-syntaxhighlighter/fileHelper.ts
+++ gulp-syntaxhighlighter/fileHelper.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-export function getFilesWithExtensionFromDir(startPath: string, filter: (path: string) => boolean, paths: string[] = []) {
+export function getFilteredFilesFromDirectoryDeep(startPath: string, filter: (path: string) => boolean, paths: string[] = []) {
     if (!fs.existsSync(startPath)) {
         console.log("no dir ", startPath);
         return paths;
@@ -12,7 +12,7 @@ export function getFilesWithExtensionFromDir(startPath: string, filter: (path: s
         var filename = path.join(startPath, files[i]);
         var stat = fs.lstatSync(filename);
         if (stat.isDirectory()) {
-            getFilesWithExtensionFromDir(filename, filter, paths);
+            getFilteredFilesFromDirectoryDeep(filename, filter, paths);
         }
         else if (filter(filename)) {
             paths.push(filename);
