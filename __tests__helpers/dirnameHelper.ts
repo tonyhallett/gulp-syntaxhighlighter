dfe6230acd7cf5a15e4cc3bd67cb3fc7af4ce841
Tony Hallett <tonyhallett74@gmail.com>
2019-03-25 10:57:33 +0000

    removed duplicate line

--- __tests__helpers/dirnameHelper.ts
+++ __tests__helpers/dirnameHelper.ts
@@ -1,7 +1,6 @@
 import * as path from 'path'
 jest.unmock('path')
 var __tests_helpersDirname=__dirname;
-path.join(__tests_helpersDirname,'../gulp-syntaxhighlighter');
 
 const expectedContainingDirectory=path.join(__tests_helpersDirname,'../gulp-syntaxhighlighter')
 export {
