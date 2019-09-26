3cc8ca2469543d013e73bdf3af1f4faa1ca9d7a4
Tony Hallett <tonyhallett74@gmail.com>
2019-09-26 17:36:36 +0100

    temp change

--- gulpfile.ts/packageJson.ts
+++ gulpfile.ts/packageJson.ts
@@ -3,6 +3,9 @@ import * as path from "path";
 export function testPackageJson(done:any){
     const packageJsonPath=path.join(path.resolve(__dirname,".."),"package.json");
     const packageJson=fs.readJSONSync(packageJsonPath);
-    console.log(packageJson.main);
+    packageJson.main="dist/madeup";
+    fs.writeJSONSync(packageJsonPath,packageJson,{
+        spaces:2
+    })
     done();
 }
